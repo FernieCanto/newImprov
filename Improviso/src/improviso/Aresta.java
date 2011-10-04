27165698155f534468bb40b14c2f554af77c8072
cfern.canto@gmail.com <cfern.canto@gmail.com@8b1cc72c-d36d-2eaf-2fcb-f937038d7d18>
2011-10-04 16:44:03 +0000

    Mais recursos adicionados

---
+++ Improviso/src/improviso/Aresta.java
@@ -0,0 +1,12 @@
+package improviso;
+import java.util.*;
+
+/**
+ *
+ * @author fernando
+ */
+public class Aresta {
+    String secaoDestino;
+    Integer probabilidade;
+    Integer maxExecucoes;
+}
---
+++ Improviso/src/improviso/Composicao.java
@@ -0,0 +1,14 @@
+package improviso;
+import java.util.*;
+
+/**
+ *
+ * @author fernando
+ */
+public class Composicao {
+    HashMap<String, Secao> secoes;
+    
+    public Composicao() {
+        secoes = new HashMap<String, Secao>();
+    }
+}
